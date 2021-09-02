931b29e06b9ec54df6760ef8ef3b98b97062125f
renshimosawa <polaris2760@gmail.com>
2021-09-02 20:57:32 +0900

    add N GM

---
+++ Gas_Notify_GM.py
@@ -0,0 +1,56 @@
+import bs4
+import time
+import requests
+import datetime
+import setting
+import schedule
+
+headers = {"User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.122 Safari/537.36"}
+
+Target_URL_S = 'https://gogo.gs/shop/0299000043'
+resp1 = requests.get(Target_URL_S, headers=headers)
+resp1.raise_for_status()
+now1 = datetime.datetime.now().strftime('%Y年%m月%d日')
+soup1 = bs4.BeautifulSoup(resp1.text, "html.parser")
+shop1 = soup1.find_all(class_="title")
+entries1 = soup1.find_all(class_="price-card")
+message_s = f'\n今日の{shop1[0].text}{entries1[0].find(class_="mode-label").text}の価格は{entries1[0].find(class_="price").text}円です'
+print( f'今日の{shop1[0].text}{entries1[0].find(class_="mode-label").text}の価格は{entries1[0].find(class_="price").text}円です')
+
+Target_URL_Y = 'https://gogo.gs/shop/0299000087'
+resp2 = requests.get(Target_URL_Y, headers=headers)
+resp2.raise_for_status()
+now2 = datetime.datetime.now().strftime('%Y年%m月%d日')
+soup2 = bs4.BeautifulSoup(resp2.text, "html.parser")
+shop2 = soup2.find_all(class_="title")
+entries2 = soup2.find_all(class_="price-card")
+message_y = f'今日の{shop2[0].text}{entries2[0].find(class_="mode-label").text}の価格は{entries2[0].find(class_="price").text}円です'
+print( f'今日の{shop2[0].text}{entries2[0].find(class_="mode-label").text}の価格は{entries2[0].find(class_="price").text}円です')
+
+message = message_y + message_s
+
+TOKEN = setting.AP_GM
+
+def main():
+    send_line_notify(
+      message
+    )
+
+def send_line_notify(notification_message):
+    """
+    LINEに通知する
+    """
+    line_notify_token = TOKEN
+    line_notify_api = 'https://notify-api.line.me/api/notify'
+    headers = {'Authorization': f'Bearer {line_notify_token}'}
+    data = {'message': f'\n{notification_message}'}
+    requests.post(line_notify_api, headers = headers, data = data)
+    time.sleep(1)
+
+if __name__ == "__main__":
+    main()
+
+schedule.every().day.at("08:00").do(main, send_line_notify)
+
+while True:
+    schedule.run_pending()
--- setting.py
+++ setting.py
@@ -5,5 +5,7 @@ from dotenv import load_dotenv
 dotenv_path = join(dirname(__file__), '.env')
 load_dotenv(dotenv_path)
 
-AP_F = os.environ.get("LINE_TOKEN_1")
-AP_Y = os.environ.get("LINE_TOKEN_2")
+AP_F = os.environ.get("LINE_TOKEN_F")
+AP_Y = os.environ.get("LINE_TOKEN_Y")
+AP_N = os.environ.get("LINE_TOKEN_N")
+AP_GM = os.environ.get("LINE_TOKEN_GM")
