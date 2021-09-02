8b486ffe62d8ee2dcc33f64a5c12319f82788acd
renshimosawa <polaris2760@gmail.com>
2021-09-02 11:55:03 +0900

    github action setting

--- Gas_Notify.py
+++ Gas_Notify.py
@@ -29,7 +29,7 @@ def send_line_notify(notification_message):
     line_notify_token = TOKEN
     line_notify_api = 'https://notify-api.line.me/api/notify'
     headers = {'Authorization': f'Bearer {line_notify_token}'}
-    data = {'message': f'/n{notification_message}'}
+    data = {'message': f'{notification_message}'}
     requests.post(line_notify_api, headers = headers, data = data)
 
 if __name__ == "__main__":
